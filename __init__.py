ca75bcec124dc59df2024b8950f24f4a061240fd
Alex Khassapov <alexbk66@hotmail.com>
2021-11-13 02:09:39 +1100

    udp works

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+from datetime import datetime
 import threading
 import time
 import socket
@@ -10,40 +11,49 @@ def thread_function(ip, port):
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     server.settimeout(0.2)
-    message = b"your very important message"
+
+    n = 0
+    
     while True:
        time.sleep(2)
-       print("Thread %s ", port) 
+       n = n + 1
+       message = "your very important message: %s" % datetime.now().strftime("%H:%M:%S")
+       message = message.encode()
+              
        server.sendto(message, (ip, port))
+
 
 class Homeseer(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        print('****************');
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
-        local_ip = s.getsockname()[0]
-        local_ip = local_ip[:local_ip.rfind('.')+1] + '255'
-        
+
+    def initialize(self):
+            
+        self.log.info("__init__");
+
+       
         broadcast = self.GetBroadcastAddr()
 
-        #x = threading.Thread(target=thread_function, args=("255.255.255.255", 10666))
-        #x = threading.Thread(target=thread_function, args=(local_ip, 10666))
+        self.speak_dialog('Broadcast %s' % broadcast);
+        
         x = threading.Thread(target=thread_function, args=(broadcast, 10666))
 
         x.start()        
 
         print('****************');
-
+        
+    
     @intent_file_handler('homeseer.intent')
     def handle_homeseer(self, message):
         self.speak_dialog('homeseer')
 
 
     def GetBroadcastAddr(self):
+        """ Get local IP and replace last bit with 255 """
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+        # connect() for UDP doesn't send packets
+        s.connect(('8.8.8.8', 1))
         local_ip = s.getsockname()[0]
         return local_ip[:local_ip.rfind('.')+1] + '255'
 
